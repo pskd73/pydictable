e578db9b502665a0aa9154a13a06e535bd177a20
ShashankJK <shashankjk18598@gmail.com>
2023-01-11 20:28:41 +0530

    adding test cases for accepting default value

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pydictable',
-    version='0.10.4',
+    version='0.11.0',
     author='Pramod Kumar',
     author_email='pramodkumar.damam73@gmail.com',
     description='Make your classes from/to dict',
